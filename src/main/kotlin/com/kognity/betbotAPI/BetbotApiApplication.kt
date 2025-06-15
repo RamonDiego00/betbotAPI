2159ad8203e3568339459384dd1f1e85b348d37b
Ramon Diego dos Santos Ferreira <ramondev001@gmail.com>
2025-06-15 14:58:23 -0300

    Initial commit

---
+++ src/main/kotlin/com/kognity/betbotAPI/BetbotApiApplication.kt
@@ -0,0 +1,11 @@
+package com.kognity.betbotAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BetbotApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BetbotApiApplication>(*args)
+}
